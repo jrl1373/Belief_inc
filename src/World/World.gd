1392ba4b94c9cbac0eb4840a441af5f0bd7ba153
jrl1373 <jrl1373@rit.edu>
2024-01-25 12:09:24 -0500

    added background map that moves

--- src/World/World.gd
+++ src/World/World.gd
@@ -3,6 +3,8 @@ extends Node2D
 var speed = .4
 var point = Vector2(0,0)
 var t = 0
+var mapspeed = 100
+var map_angle = randf()*2*PI
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Advisor.play()
@@ -12,6 +14,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	move_advisor(delta)
+	move_map(delta)
+	
+	pass
+func move_map(delta):
+	var width = $Map.texture.get_width()
+	var height = $Map.texture.get_height()
+	if $Map.position.x > width/2:
+		map_angle+=PI/2
+	if $Map.position.x < -1*((width/2)-get_window().size.x):
+		map_angle+=PI/2
+	if $Map.position.y > height/2:
+		map_angle+=PI/2
+	if $Map.position.y < -1*((height/2)-get_window().size.y):
+		map_angle+=PI/2
+	$Map.position += mapspeed*delta * Vector2(1,1).rotated(map_angle)
+
+func move_advisor(delta):
 	$Advisor.rotate(1*delta)
 	t+=speed*delta
 	if Input.is_action_pressed("click"):
@@ -19,9 +39,9 @@ func _process(delta):
 		t = 0
 	if point != Vector2(0,0):
 		$Advisor.position = $Advisor.position.lerp(point,t)
-		
-	pass
-
+	
+	
+	
 
 func _on_timer_timeout():
 	var atr = $Ideology.attributes
